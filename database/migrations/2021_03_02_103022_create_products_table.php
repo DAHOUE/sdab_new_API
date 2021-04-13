4b09f0a1556ae135fb1a766b8752502773dd4723
Isaac <isaacdahoue1@gmail.com>
2021-04-13 13:46:40 +0100

    development of api

---
+++ database/migrations/2021_03_02_103022_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->float('price_achat', 8, 2)->nullable();
+            $table->float('price_vente', 8, 2)->nullable();
+            $table->integer('quantity')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
